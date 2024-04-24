159b312a2721b2e26ad036023ca6cba82d91eb8a
Cauã Farias <cauafariasdev@gmail.com>
2024-04-24 17:29:55 -0300

    Inicial Commit, feat: add task class

---
+++ src/main/java/com/cauzy/tasklist/entities/Task.java
@@ -0,0 +1,64 @@
+package com.cauzy.tasklist.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_task")
+public class Task implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Instant deadLine;
+
+    public Task() {
+    }
+
+    public Task(Long id, String description, Instant deadLine) {
+        this.id = id;
+        this.description = description;
+        this.deadLine = deadLine;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Instant getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(Instant deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
